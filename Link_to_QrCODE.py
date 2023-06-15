2cf50d9d052d52d22b3cc55fc9c73f9441bc04fa
neosandeep24 <103883917+neosandeep24@users.noreply.github.com>
2023-06-15 10:58:08 +0530

    Add files via upload

---
+++ Link_to_QrCODE.py
@@ -0,0 +1,12 @@
+import pyqrcode
+import png
+from pyqrcode import QRCode
+  
+
+s = "https://github.com/neosandeep24/Link_to_QR-code-converter"
+
+url = pyqrcode.create(s)
+
+url.svg("myqr.svg", scale = 8)
+  
+url.png('myqr.png', scale = 6)
